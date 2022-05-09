daa6b2ddf9a20449c3eed5466bc6854dd0a4cbfb
Егор Иванов <iwanov.eg@gmail.com>
2022-05-09 11:30:26 +0300

    fix task 2

--- task02-slice.go
+++ task02-slice.go
@@ -3,7 +3,7 @@ package homework
 func reverse(input []int64) (result []int64) {
 	var maxIndex = len(input) - 1
 	for i := range input {
-		result[i] = input[maxIndex-i]
+		result = append(result, input[maxIndex-i])
 	}
 	return
 }
